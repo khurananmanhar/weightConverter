becc5b529391d277ff27ae9442c41ad6084e517b
khuranamanhar <72623458+khurananmanhar@users.noreply.github.com>
2021-01-18 16:55:06 -0500

    weightConverter

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+let og = document.getElementById('og')
+let newWeight = document.getElementById('new')
+let kgToLb = () => {
+    let value = document.getElementById('weight').value;
+    og.innerText = value
+    newWeight.innerText = (value * 2.2046226218).toFixed(2)
+}
+let lbToKg = () => {
+    let value = document.getElementById('weight').value;
+    og.innerText = value
+    newWeight.innerText = (value / 2.2046226218).toFixed(2)
+}
